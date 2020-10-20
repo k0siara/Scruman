3c61906bf182717978e4ca9630242ed27f827a90
k0siara <patryk.kosieradzki@gmail.com>
2020-10-20 21:37:16 +0200

    Added travis config

--- src/main/java/com/scruman/backend/service/ProjectService.java
+++ src/main/java/com/scruman/backend/service/ProjectService.java
@@ -14,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
